f17560a855e5140e3edc0cdbc9f3ac6ec589fb68
jacob <jacob@DESKTOP-6PELQJ4>
2022-07-23 20:06:14 +0100

    Add project files.

---
+++ RandomSteamGame/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Management;
+using System.Diagnostics;
+
+namespace RandomSteamGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ChooseRandomGame()
+        {
+            Random rng = new Random();
+            string chosen = GameLoader.gameDirectories.ElementAt(rng.Next(0, GameLoader.gameDirectories.Count));
+            Process.Start(chosen);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChooseRandomGame();
+        }
+    }
+}
---
+++ RandomSteamGame/GameLoader.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+
+namespace RandomSteamGame
+{
+    public static class GameLoader
+    {
+        private const string KeyName = @"HKEY_CURRENT_USER\SOFTWARE\Valve\steam";
+        static readonly string SteamDir = Registry.GetValue(KeyName, "Steamexe", " ").ToString().Replace("steam.exe","steamapps");
+
+       private static List<string> installDirectories = findInstallDirectories();
+       public static List<string> gameDirectories = findInstalledGames();
+
+        public static string getSteamDirectory()
+        {
+            return SteamDir;
+        }
+
+        private static List<String> findInstallDirectories()
+        {
+            List<string> installDirectories = new List<string>();
+
+            foreach (string dir in Directory.GetFiles(SteamDir))
+            {
+                if (dir.Contains(".acf"))
+                {
+                    var temp = File.OpenText(dir);
+                    string currentInstallDirectory = temp.ReadToEnd();
+                    currentInstallDirectory = currentInstallDirectory.Split("\n").ElementAt(7);
+                    currentInstallDirectory = currentInstallDirectory.Split("\"").ElementAt(3);
+                    temp.Close();
+                    currentInstallDirectory = currentInstallDirectory.Insert(0, (SteamDir + "/common/"));
+                    installDirectories.Add(currentInstallDirectory);
+                } 
+            }
+            return installDirectories;
+        }
+
+
+        static string getRunPath(string path)
+        {
+            string searchPattern = "*.exe";
+            DirectoryInfo di = new DirectoryInfo(path);
+      
+            return di.GetFiles(searchPattern, SearchOption.AllDirectories)[0].ToString(); 
+        }
+
+        public static List<string> findInstalledGames()
+        {
+            List<string> gameList = new List<string>();
+
+            foreach (string directory in installDirectories)
+            {
+                gameList.Add(getRunPath(directory));
+            }
+
+            return gameList;
+        }
+
+    }
+
+
+
+}
+
